7db580da0e4bdde7c07c2ed7c3c491a52078cdf0
chandana505 <84139704+chandana505@users.noreply.github.com>
2025-11-09 01:33:51 +0530

    replaced price

--- app/models.py
+++ app/models.py
@@ -6,5 +6,5 @@ class Book(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     title = Column(String(255), nullable=False)
     author = Column(String(255), nullable=False)
-    price_cents = Column(Integer, nullable=False, default=0)
+    price = Column(Integer, nullable=False, default=0)
     stock = Column(Integer, nullable=False, default=0)
